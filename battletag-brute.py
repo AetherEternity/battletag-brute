1a89712237e289c1e8d97ce7e06def2bc0897424
AetherEternity <swing056@gmail.com>
2017-12-13 11:26:42 +0300

    Initial commit

---
+++ battletag-brute.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2
+# coding: utf-8
+# AetherEternity, 2017
+# https://
+
+# Enable dialogue mode
+switch=1
+if (switch):
+	print 'Enter nickname:'
+	targetName=raw_input()
+	print ''
+	print 'Enter region:'
+	print '1=US, 2=EU, 3=KR, 5=CN'
+	targetRegion=raw_input()
+	print ''
+	print 'Stop on success? (1/0)'
+	allvars=int(raw_input())
+	print ''
+else:
+	# Nickame (String before #)
+	targetName='Kripp'
+	# Region (1=US, 2=EU, 3=KR, 5=CN)
+	targetRegion=2
+	# Try all variants?
+	allvars=0
+success=0
+
+import requests 
+for val in range(10000):
+	r = requests.get('https://api.hotslogs.com/Public/Players/'+str(targetRegion)+'/'+targetName+'_'+str(targetRegion)+str(val))
+	if r.text!='null':
+		print 'Found: '+targetName+'#'+str(targetRegion)+str(val)
+		success=1
+		if allvars:
+			exit()
+if success==0:
+	print 'Failed'
